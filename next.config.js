7b86fbceedd2032c14b20b25ae9c24fa17763de9
oyeitsmeow <zhongzichenn@gmail.com>
2024-11-08 20:14:31 +0800

    chore: add analyze

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack: function(config) {
+  webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
-  }
-}
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
