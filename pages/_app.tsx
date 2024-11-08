17c4d6f543508f250a3a4b4cb2f7d66a494c73d5
oyeitsmeow <zhongzichenn@gmail.com>
2024-11-08 11:52:40 +0800

    fix: remove loading display font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,10 @@
 import "../styles/variable.css";
 import "../styles/globals.css";
 import "../styles/github-markdown.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import type { AppProps } from "next/app";
 
-const useLoadFontFace = () => {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    // Listen for the font loading status
-    const checkFonts = async () => {
-      await document.fonts.ready; // This ensures all fonts are loaded
-      setFontsLoaded(true);
-    };
-
-    checkFonts();
-  }, []);
-
-  return [fontsLoaded];
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const [fontsLoaded] = useLoadFontFace();
-
-  if (!fontsLoaded) {
-    return <>Loading...</>;
-  }
   return <Component {...pageProps} />;
 }
 
