2d35513875d24ff6ac7f4687b7f423659d5040bb
oyeitsmeow <zhongzichenn@gmail.com>
2024-11-08 11:29:08 +0800

    fix: remove js load font-face

--- README.md
+++ README.md
@@ -28,3 +28,30 @@ yarn start
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Q&A
+
+1. Build failed because of webpack errors
+
+```bash
+error - Failed to patch lockfile, please try uninstalling and reinstalling next in this workspace
+TypeError: fetch failed
+    at node:internal/deps/undici/undici:12618:11
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at async Object.patchIncorrectLockfile (/Users/zhongzichen/Develop/zzcyes-github/nextjs-blog/node_modules/next/dist/lib/patch-incorrect-lockfile.js:72:33)
+    at async Object.build [as default] (/Users/zhongzichen/Develop/zzcyes-github/nextjs-blog/node_modules/next/dist/build/index.js:1350:9) {
+  cause: ConnectTimeoutError: Connect Timeout Error
+      at onConnectTimeout (node:internal/deps/undici/undici:7760:28)
+      at node:internal/deps/undici/undici:7716:50
+      at Immediate._onImmediate (node:internal/deps/undici/undici:7748:13)
+      at process.processImmediate (node:internal/timers:476:21) {
+    code: 'UND_ERR_CONNECT_TIMEOUT'
+  }
+}
+
+> Build failed because of webpack errors
+```
+
+When on an M1 Mac and switching from a Node.js version without M1 support to one with, e.g. v14 to v16, you may need a different swc dependency which can require re-installing node_modules (npm i --force or yarn install --force).
+
+https://nextjs.org/docs/messages/failed-loading-swc
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,51 +4,25 @@ import "../styles/github-markdown.css";
 import React, { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 
-const useLoadFontFace = ({
-  urls,
-}: {
-  urls: Array<{
-    url: string;
-    fontFamily: string;
-  }>;
-}) => {
+const useLoadFontFace = () => {
   const [fontsLoaded, setFontsLoaded] = useState(false);
+
   useEffect(() => {
-    const loadFonts = async () => {
-      const fontPromises = urls.map(({ url, fontFamily }) => {
-        const font = new FontFace(fontFamily, `url(${url})`);
-        return font.load().then(() => {
-          document.fonts.add(font);
-        });
-      });
-      await Promise.all(fontPromises);
+    // Listen for the font loading status
+    const checkFonts = async () => {
+      await document.fonts.ready; // This ensures all fonts are loaded
       setFontsLoaded(true);
     };
-    loadFonts();
-  }, [urls]);
 
-  return {
-    fontsLoaded,
-  };
+    checkFonts();
+  }, []);
+
+  return [fontsLoaded];
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { fontsLoaded } = useLoadFontFace({
-    urls: [
-      {
-        fontFamily: "CuteAlphabet",
-        url: "/font-split/CuteAlphabet.woff2",
-      },
-      {
-        fontFamily: "PuHuiTi",
-        url: "/font-split/Alibaba-PuHuiTi-Regular.ttf",
-      },
-      {
-        fontFamily: "Handdrawn",
-        url: "/font-split/Handdrawn.ttf",
-      },
-    ],
-  });
+  const [fontsLoaded] = useLoadFontFace();
+
   if (!fontsLoaded) {
     return <>Loading...</>;
   }
