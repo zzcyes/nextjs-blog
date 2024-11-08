3d68df2f2c5d25c167b7cf0ce8285fbd38b66fc5
oyeitsmeow <zhongzichenn@gmail.com>
2024-11-08 13:02:24 +0800

    fix: update toplist style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/variable.css";
 import "../styles/globals.css";
 import "../styles/github-markdown.css";
+import "../styles/navbar.css";
+
 import React from "react";
 import type { AppProps } from "next/app";
 
